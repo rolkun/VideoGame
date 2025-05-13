1eed7477a4b67e2a2000f29b68009dacca4fd3ff
Roland Kunz <roland.kunz@erowa.com>
2025-05-13 16:03:54 +0200

    add video game controller

---
+++ VideoGame.WebApi/Controllers/VideoGameController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VideoGame.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGameController : ControllerBase
+    {
+        static private List<VideoGame> videoGames = new List<VideoGame>
+        {
+            new VideoGame
+            {
+                Id = 1, Title = "The Legend of Zelda: Breath of the Wild",
+                Platform = "Nintendo Switch",
+                Developer = "Nintendo",
+                Publisher = "Nintendo"
+            },
+            new VideoGame
+            {
+                Id = 2,
+                Title = "The Witcher 3: Wild Hunt",
+                Platform = "PC",
+                Developer = "CD Projekt Red",
+                Publisher = "CD Projekt"
+            },
+            new VideoGame
+            {
+                Id = 3,
+                Title = "Dark Souls III",
+                Platform = "PC",
+                Developer = "FromSoftware",
+                Publisher = "Bandai Namco Entertainment"
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<VideoGame>> GetAllVideoGames()
+        {
+            return Ok(videoGames);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<VideoGame> GetVideoGameById(int id)
+        {
+            var videoGame = videoGames.FirstOrDefault(v => v.Id == id);
+            if (videoGame == null)
+            {
+                return NotFound();
+            }
+            return Ok(videoGame);
+        }
+    }
+}
